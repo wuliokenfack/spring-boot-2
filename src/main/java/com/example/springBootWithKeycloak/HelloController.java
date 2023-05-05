964f21882d7644af674c510a07f7f7639250be7a
Wulio Fosso <fossokenfack@yahoo.com>
2023-05-05 01:54:06 -0500

    first commit

---
+++ src/main/java/com/example/springBootWithKeycloak/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.springBootWithKeycloak;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/test")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
+
